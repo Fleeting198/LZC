16860cff0b50e4bd7a1f0abcbc286c49fcf44834
Fleeting198 <646335299@qq.com>
2016-02-10 22:06:07 +0800

    First functional example

--- app/views.py
+++ app/views.py
@@ -11,11 +11,16 @@ def index():
     return render_template('base.html')  # 模版路径名一定要写全
 
 
+# 仅仅加载图表，json传递由js发起。
+@app.route('/expenditure')
+def chart_expenditure():
+    return render_template('chart-expenditure.html')
+
+
 # 传给前端的数据格式：
 # 'data':[消费金额按时间顺序],
-@app.route('/expenditure', methods=['GET'])
-def chart_expenditure():
-    # from decimal import Decimal
+@app.route('/expenditure/refresh', methods=['GET'])
+def data_expenditure():
 
     # 获取参数
     user_id = request.args.get('user_id')
@@ -31,18 +36,18 @@ def chart_expenditure():
         # print len(results)
 
         # 构造日期数组作为图表x轴标记
-        date = [result.con_datetime for result in results]
+        mdates = [result.con_datetime for result in results]
 
         # 构造对应的消费值
         # data = [float(Decimal(result.amount).to_eng_string()) for result in results]
-        data = [float(result.amount) for result in results]
+        mamounts = [float(result.amount) for result in results]
 
         # 构造返回json
         try:
-            str_response = {"date": date, "data": data}
+            str_response = {"date": mdates, "data": mamounts}
             # json_response = jsonify(str_response)
-            json_response = jsonify(date=date, data=data)
+            json_response = jsonify(mdates=mdates, mamounts=mamounts)
         except Exception,e:
             print e
 
-    return render_template('chart-expenditure.html', json_response=json_response)
+    return json_response
