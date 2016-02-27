8de07e36e118ccaf59c3d53edba2438bae836510
Fleeting198 <646335299@qq.com>
2016-02-27 17:26:23 +0800

    Merge L into C  conflicts solved

--- app/views.py
+++ app/views.py
@@ -649,11 +649,3 @@ def refresh_chart_penalty():
     else:
         json_response = jsonify(errMsg=form.errors)
     return json_response
-<<<<<<< HEAD
-=======
-
-
-@app.route('/summary')
-def show_summary():
-    return render_template('summarization.html')
->>>>>>> L
